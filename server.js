a39d9fa0f7f9b8a85e3535717edb953392e2d7e3
Tommy T <tt@users.noreply.github.com>
2016-02-09 22:29:44 +0800

    added fb params

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ var SampleApp = function() {
                 //passReqToCallback : true,
                 //profileFields: ['id', 'emails', 'name'] 
                 //profileFields: ['emails'] 
-                profileFields: ['id', 'displayName', 'emails']
+                profileFields: ['id', 'displayName', 'emails', 'email']
             },
             function(accessToken, refreshToken, profile, done) {
                 
@@ -167,6 +167,7 @@ var SampleApp = function() {
                                 email: profile.emails[0].value,
                                 //username: profile.username,
                                 fbid: profile.id,
+                                facebook: profile.email
                                 //fbid: profile._json.id,
                                 //facebook: profile._json
                                 //createDate: new Date(),
@@ -207,7 +208,7 @@ var SampleApp = function() {
         // Redirect the user to Facebook for authentication.  When complete,
         // Facebook will redirect the user back to the application at
         //     /auth/facebook/callback
-        app.get('/auth/facebook', passport.authenticate('facebook', { authType: 'rerequest', scope: ['email', 'basic_info']}));
+        app.get('/auth/facebook', passport.authenticate('facebook', { authType: 'rerequest', scope: ['email']}));
         //app.get('/auth/facebook', passport.authenticate('facebook'));
 
         // Facebook will redirect the user to this URL after approval.  Finish the
