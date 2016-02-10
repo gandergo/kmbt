c5e14fd5482d93ea182162549b3e5952940d5bcb
Tommy T <tt@users.noreply.github.com>
2016-02-10 16:59:19 +0800

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'KMB Songs' });
+  res.render('index', { title: 'KMB Songs', uid: req.user.fbId });
 });
 
 module.exports = router;
