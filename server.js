0c48eaf33d585d4f946e9f61fc84093a2eff1af0
gander <gander>
2017-09-07 21:05:53 +0800

    seperate

--- server.js
+++ server.js
@@ -278,7 +278,7 @@ var SampleApp = function() {
 		console.log('url = ', url);
 
         var mongoose = require('mongoose');
-        mongoose.connect(url, function(err) {
+        mongoose.connect(url, , { user: process.env.OPENSHIFT_MONGODB_DB_USER, pass: process.env.OPENSHIFT_MONGODB_DB_PASS}, function(err) {
             if(err) {
                 console.log('connection error', err);
             } else {
